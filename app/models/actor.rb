60867f1348791db1221b2842799ff00226db57f2
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-04-14 06:12:04 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
   end 
 
   def list_roles
-    self.characters.map {|character|}
+    self.characters.map {|character| "#{character.name} - #{character.show.name}"}
     roles = []
     self.characters.each do |x|
         roles << "#{x.name}"+" - "+"#{x.show.name}"
